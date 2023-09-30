9913d9355ed3a4b398c210141879c7025e78d222
sspccreates <145820126+sspccreates@users.noreply.github.com>
2023-09-29 22:51:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function shortenUrl() {
   const longUrl = document.getElementById('longUrl').value;
-  const accessToken = '47ca96c9161824368d379d767d2b8ef50b527902';
+  const customSlug = document.getElementById('customSlug').value;
+  const accessToken = '47ca96c9161824368d379d767d2b8ef50b527902'; // Your Bitly access token
 
   if (!longUrl || !accessToken) {
     alert('Please enter a URL and access token.');
     return;
   }
 
-  const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+  let apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+
+  if (customSlug) {
+    apiUrl += `?custom_bitlink=${encodeURIComponent(customSlug)}`;
+  }
 
   fetch(apiUrl, {
     method: 'POST',
@@ -19,18 +24,10 @@ function shortenUrl() {
       long_url: longUrl
     })
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
+  .then(response => response.json())
   .then(data => {
     const shortUrl = data.link;
     document.getElementById('shortUrl').innerHTML = `<strong>Shortened URL:</strong> <a href="${shortUrl}" target="_blank">${shortUrl}</a>`;
   })
-  .catch(error => {
-    document.getElementById('shortUrl').innerHTML = `Error: ${error.message}`;
-    console.error('Error:', error);
-  });
+  .catch(error => console.error('Error:', error));
 }
